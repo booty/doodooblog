28a3b2fdfee6a5e6d135222f502a19ed82d58282
booty <johnedmundrose@gmail.com>
2019-02-05 10:07:06 -0500

    Create and display comments (completed 6.4)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  has_many :comments
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :text, presence: true, length: { minimum: 10 }
 end
